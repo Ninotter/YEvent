cbb0f627ab9dc7b7d9d2fc72809dfe48c66d3d60
Nino <nino.didier@live.fr>
2025-01-24 12:28:34 +0100

    cleaned up file

--- app/(tabs)/homeScreen.tsx
+++ app/(tabs)/homeScreen.tsx
@@ -13,7 +13,7 @@ export default function HomeScreen({navigation}: any) {
   const [loading, setLoading] = useState(false);
 
   async function getEvents() {
-    setLoading(true); // Indicate loading state
+    setLoading(true);
     try {
       const { data, error, status } = await supabase
         .from('Evenements')
@@ -23,7 +23,6 @@ export default function HomeScreen({navigation}: any) {
       }
 
       if (data) {
-         // Map the raw JSON data into an array of YEvent
         const events: YEvent[] = data.map((item: any) => ({
           id: item.id,
           titre: item.titre,
@@ -40,17 +39,15 @@ export default function HomeScreen({navigation}: any) {
         console.error(error.message)
       }
     } finally {
-      setLoading(false); // Stop loading state
+      setLoading(false);
     }
   }
 
    // Use useEffect to control when getEvents is called
    useEffect(() => {
-    getEvents(); // Fetch events when the component mounts
-  }, []); // Empty dependency array ensures this runs only once
+    getEvents();
+  }, []);
 
-  // const latestReleases = getMockDataLatestReleases();
-  // const fewTicketsLeft = getMockDataFewTicketsLeft();
   return (
     <ScrollView
       style={{
@@ -95,7 +92,7 @@ export default function HomeScreen({navigation}: any) {
             Dernières annonces
           </Text>
           {loading ? (
-            <Text>Loading...</Text>
+            <Text style={{flex : 1}}>Loading...</Text>
           ) : (
             <YEventsList events={latestReleases} navigation={navigation} />
           )}
@@ -108,73 +105,3 @@ export default function HomeScreen({navigation}: any) {
     </ScrollView>
   );
 }
-
-// function getMockDataFewTicketsLeft(): Array<YEvent> {
-//   return [
-//     {
-//       id: 1,
-//       titre: "Concert de rock",
-//       date: "2021/10/15",
-//       location: "Paris, 5 rue des musiciens",
-//       price: 20,
-//       maxCapacity: 100,
-//       leftCapacity: 10,
-//       description: "Venez écouter les plus grands classiques",
-//     },
-//     {
-//       id: 2,
-//       titre: "Exposition de peinture",
-//       date: "2021/10/20",
-//       location: "Lyon, 3 rue des artistes",
-//       price: 10,
-//       maxCapacity: 50,
-//       leftCapacity: 5,
-//       description: "Venez découvrir les plus belles œuvres",
-//     },
-//     {
-//       id: 3,
-//       titre: "Conférence sur l'écologie",
-//       date: "2021/10/25",
-//       location: "Marseille",
-//       price: 0,
-//       maxCapacity: 1000,
-//       leftCapacity: 100,
-//       description: "Venez échanger sur les enjeux actuels",
-//     }
-//   ]
-// }
-
-// function getMockDataLatestReleases(): Array<YEvent> {
-//   return [
-//     {
-//       id: 1,
-//       titre: "Concert de musique classique",
-//       date: "2021/10/15",
-//       location: "Paris, 5 rue des musiciens",
-//       price: 20,
-//       maxCapacity: 100,
-//       leftCapacity: 50,
-//       description: "Venez écouter les plus grands classiques",
-//     },
-//     {
-//       id: 2,
-//       titre: "Exposition de peinture",
-//       date: "2021/10/20",
-//       location: "Lyon, 3 rue des artistes",
-//       price: 10,
-//       maxCapacity: 50,
-//       leftCapacity: 0,
-//       description: "Venez découvrir les plus belles œuvres",
-//     },
-//     {
-//       id: 3,
-//       name: "Conférence sur l'écologie",
-//       date: "2021/10/25",
-//       location: "Marseille, en ligne",
-//       price: 0,
-//       maxCapacity: 1000,
-//       leftCapacity: 1000,
-//       description: "Venez échanger sur les enjeux actuels",
-//     }
-//   ];
-// }
